75c181a204959147342513853d9858554d83b123
your_username <sardaralimuradali4@gmail.com>
2023-01-19 02:02:55 +0500

    removing the gapi

--- components/GoogleLogin.js
+++ components/GoogleLogin.js
@@ -3,20 +3,20 @@ import { GoogleLogin } from 'react-google-login';
 import axios from "axios"
 // import { gapi } from "gapi-script";
 import { useAppContext } from '../context/appContext';
-import { gapi } from "gapi-script";
+// import { gapi } from "gapi-script";
 
 
 export default function Landing() { 
   let { Google,user}=useAppContext()
 
 
-  gapi.load("client:auth2", () => {
-    gapi.client.init({
-      client_id:"705164632277-vhv4q8ki9tntsbiv8n0do8l9rdbd1knk.apps.googleusercontent.com",
-      plugin_name: "chat",
-      apiKey: "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",
-    });
-  });
+  // gapi.load("client:auth2", () => {
+  //   gapi.client.init({
+  //     client_id:"705164632277-vhv4q8ki9tntsbiv8n0do8l9rdbd1knk.apps.googleusercontent.com",
+  //     plugin_name: "chat",
+  //     apiKey: "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",
+  //   });
+  // });
  
    const responseGoogleSuccess = async (response) => {
        try {
